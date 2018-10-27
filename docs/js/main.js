9a82f98071292c8b182783d2bec6e87ec3e1f174
Mariona Roca <mariona.roca@gmail.com>
2018-10-27 16:17:50 +0200

    modal i part de l'slider

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,34 @@
-window.addEventListener('load', init);
+window.addEventListener('load', start);
 
-const init = () => {
-  document.querySelector('.call-to-action').addEventListener('click', modal);
+function start() {
+  console.log('init');
+  document.querySelector('.call-to-action').addEventListener('click', modalIn);
+  document.querySelector('.modal .close').addEventListener('click', modalOut)
+  slider();
 }
 
-const modal = () => {
+function modalIn() {
+  console.log('modal');
   document.querySelector('.modal').style.top='100px';
+  document.querySelector('.modal').style.transition='all 1s';
+  document.querySelector('.modal .overlay').style.display='block';
+}
+function modalOut() {
+  console.log('modalOut');
+  document.querySelector('.modal').style.top='-600px';
+  document.querySelector('.modal').style.transition='all 1s';
+  document.querySelector('.modal .overlay').style.display='none';
+}
+
+var sliderNum = 1;
+function slider() {
+  console.log('slider');
+  setInterval(1000, moveSlide);
+}
+function moveSlide(){
+  document.querySelector('slide1').style.left = width*(sliderNum);
+  document.querySelector('slide2').style.left = width*(sliderNum+1);
+  document.querySelector('slide3').style.left = width*(sliderNum+2);
+  document.querySelector('slide4').style.left = width*(sliderNum+3);
+  (sliderNum++)%4;
 }
